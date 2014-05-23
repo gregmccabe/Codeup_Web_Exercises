9b126396e967ab9bff3e8b6c9097f8928f9d74d6
Greg McCabe <gsmccabe1@gmail.com>
2014-05-23 10:56:19 -0500

    html forms log in and email form 1st version

--- public/forms.php
+++ public/forms.php
@@ -10,6 +10,7 @@
 		?>
 <body>
 	<form method="POST">
+		<h3>User Log In</h3>
     	<p>
 	        <label for="username">Username</label>
 	        <input id="username" name="username" type="text" placeholder="User Name">
@@ -21,6 +22,29 @@
 	    <p>
 	        <button type="Submit" name="Submit">Log In</button>
 	    </p>
+	</form>
+	<form method="POST">
+		<p>
+			<h3>Compose Email</h3>
+		</p>
+		<p>
+			<lable for='to'>To</lable>
+			<input id="to" name="to" type="text">
+		</p>
+		<p>
+			<lable for='from'>From</lable>
+			<input id="from" name="from" type="text">
+		</p>
+		<p>
+			<lable for='Subject'>Subject</lable>
+			<input id="Subject" name="Subject" type="text">
+		</p>
+		<p>
+			<textarea id="email_body" name="email_body" rows="20" cols="100"></textarea>
+		</p>
+		<p>
+			<button type="Submit" name="Submit">Send</button>
+		</p>
 	</form>
 </body>
 
