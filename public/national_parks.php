6a394a4c686aaf7f477b7cbc997f65375027df3b
Greg McCabe <gsmccabe1@gmail.com>
2014-06-23 16:24:01 -0500

    national parks final

--- public/national_parks.php
+++ public/national_parks.php
@@ -5,46 +5,79 @@ $dbc = new PDO('mysql:host=127.0.0.1;dbname=codeup_pdo_test_db', 'greg', 'letmei
 // Tell PDO to throw exceptions on error
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+$dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
 
+function getOffset() {
+    $page = isset($_GET['page']) ? $_GET['page'] : 1;
+    return ($page - 1) * 4;
+}
 
 $limitRecord = 4;
 $pageNumber = 0;
+$offset = 0;
 
-$query = "SELECT * FROM national_parks LIMIT {$limitRecord} OFFSET {$pageNumber}";
+if (isset($_GET['page'])) {
+    $pageNumber=$_GET['page'];
+    $offset = $pageNumber * $limitRecord;
+
+}
+
+$query = "SELECT * FROM national_parks LIMIT {$limitRecord} OFFSET {$offset}";
 $stmt = $dbc->query($query);
-
 $parks = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// var_dump($parks);
+$count = $dbc->query("SELECT * FROM national_parks;")->fetchColumn();
+
+$numPage = ceil($count / $limitRecord);
+
+$nextPage = $pageNumber + 1;
+$prevPage = $pageNumber - 1;
+
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
     <meta charset="UTF-8">
     <title>national parks</title>
 </head>
 <body>
-    <h1>National Parks</h1>
-    <table border="1">
-        <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Location</th>
-            <th>Date</th>
-            <th>Acres</th>
-        </tr>
-         <? foreach ($parks as $park) : ?>
-        <tr>
-            <? foreach ($park as $key => $value) : ?>
-            <td><?= htmlspecialchars(strip_tags($value)); ?></td>
-            <? endforeach; ?>
-        </tr>
-            <? endforeach; ?>
-    </table>
-    <a href=>page forward</a>
-    <a href=>page back</a>
+<div class="container">
+    <h1 align="center">National Parks</h1>
+        <table class="table table-striped"border="1">
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Location</th>
+                <th>Date</th>
+                <th>Acres</th>
+            </tr>
+             <? foreach ($parks as $park) : ?>
+            <tr>
+                <? foreach ($park as $key => $value) : ?>
+                <td><?= htmlspecialchars(strip_tags($value)); ?></td>
+                <? endforeach; ?>
+            </tr>
+                <? endforeach; ?>
+        </table>
+
+    <ul class="pager">
+        <? if ($pageNumber > 0) : ?>
+          <li class="previous"><a href="national_parks.php?page=<?= $prevPage; ?>">&larr; Previous</a></li>
+        <? endif ?>
+
+        <? if ($pageNumber <= $numPage) : ?>
+          <li class="next"><a href="national_parks.php?page=<?= $nextPage; ?>">Next &rarr;</a></li>
+        <? endif ?>
+    </ul>
+</div>
 </body>
-</html>
+</html
+
+
